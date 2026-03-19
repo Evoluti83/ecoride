d33b71e488400f189ce276643f02f235248f9898
Evoluti83 <bauduin.teddy62@gmail.com>
2026-03-19 14:42:13 +0100

    feat: configuration déploiement Heroku

--- config/database.php
+++ config/database.php
@@ -1,22 +1,27 @@
 <?php
-
-$host = "127.0.0.1";
-$dbname = "ecoride";
-$user = "root";
-$password = "";
+// En production (Heroku) on utilise JAWSDB_URL
+// En local on utilise les variables directes
+if (getenv('JAWSDB_URL')) {
+    $url = parse_url(getenv('JAWSDB_URL'));
+    $host     = $url['host'];
+    $dbname   = ltrim($url['path'], '/');
+    $user     = $url['user'];
+    $password = $url['pass'];
+} else {
+    // Config locale XAMPP
+    $host     = "127.0.0.1";
+    $dbname   = "ecoride";
+    $user     = "root";
+    $password = "";
+}
 
 try {
-
     $pdo = new PDO(
         "mysql:host=$host;dbname=$dbname;charset=utf8mb4",
         $user,
         $password
     );
-
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
 } catch (PDOException $e) {
-
-    die("Erreur de connexion à la base de données : " . $e->getMessage());
-
+    die("Erreur de connexion : " . $e->getMessage());
 }
